3eaf82f3b6bc9882879bd8d208b8b6ae3afb0d11
Kevin-tech19 <Hhj680158@gmail.com>
2025-06-12 16:00:06 +0530

    Add files via upload

---
+++ mega.js
@@ -0,0 +1,37 @@
+const mega = require("megajs");
+
+const auth = {
+    email: 'petersuccessful73@gmail.com',   //use your real vaild mega account email
+    password: 'Aiyeki.67',  ////use your real vaild mega account password
+    userAgent: 'Mozilla/5.0(Linux; Android 10; K) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36'
+};
+
+const upload = (data, name) => {
+    return new Promise((resolve, reject) => {
+        try {
+            if (!auth.email || !auth.password || !auth.userAgent) {
+                throw new Error("Missing required authentication fields");
+            }
+
+            console.log("Using auth:", auth); // Debugging line
+
+            const storage = new mega.Storage(auth, () => {
+                data.pipe(storage.upload({ name: name, allowUploadBuffering: true }));
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        storage.close();
+                        resolve(url);
+                    });
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+module.exports = { upload };
